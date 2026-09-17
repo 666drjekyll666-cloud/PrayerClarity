e2ec1b303695c8f1700119792177cfa4f7be5cce
666drjekyll666-cloud <666drjekyll666@gmail.com>
2026-09-17 12:51:48 +0300

    release: bump PrayerClarity to 1.0.19

--- src/PrayerClarityPlugin.cs
+++ src/PrayerClarityPlugin.cs
@@ -10,7 +10,7 @@ namespace PrayerClarity
     {
         internal const string PluginGuid = "nikich.graveyardkeeper.prayerclarity";
         internal const string PluginName = "PrayerClarity";
-        internal const string PluginVersion = "1.0.18";
+        internal const string PluginVersion = "1.0.19";
         private static readonly Guid SupportedGameMvid = new Guid("6f50b8e7-156b-49ac-bbe8-7505894b2364");
         private static ManualLogSource _log;
         private static bool _runtimeErrorLogged;
